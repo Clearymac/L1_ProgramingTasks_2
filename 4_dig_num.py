e04d2c662b3aab9aeaad2cd8c68cc90e536be61a
Clearymac <clearyd@stu.otc.school.nz>
2021-04-01 09:13:28 +1300

    wrong

--- 4_dig_num.py
+++ 4_dig_num.py
@@ -1,7 +1,15 @@
+#making the code input a var
 code = int(input("Please enter a 4 digit code: "))
 
 for hack in range(9999):
-    print(hack)
+    if hack < 1000 and hack > 99:
+        print("0" + hack)
+    if hack < 100  and hack > 9:
+        print("00" + hack)
+    if hack < 10:
+        print("000" + hack)
+    if hack > 999:
+        print(hack)
     if hack == code:
         print("The code is {}".format(code))
         break
