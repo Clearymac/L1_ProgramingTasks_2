f003dfef769d997d4a5e6ca18084d0155281758f
Clearymac <clearyd@stu.otc.school.nz>
2021-04-01 09:34:04 +1300

    wrong

--- 4_dig_num.py
+++ 4_dig_num.py
@@ -1,15 +1,28 @@
-#making the code input a var
+#making the code input a var and makes sure retard enters a 4 digit code
 code = int(input("Please enter a 4 digit code: "))
 
-for hack in range(9999):
+while code < 0 or code > 9999:
+    code = int(input("Please enter a 4 digit code: "))
+if code < 0 or code > 9999:
+        print("Your mum suck me good and hard through my jorts")
+
+print(code)
+
+for hack in range(10000):
     if hack < 1000 and hack > 99:
-        print("0" + hack)
+        print("0{}".format(hack))
     if hack < 100  and hack > 9:
-        print("00" + hack)
+        print("00{}".format(hack))
     if hack < 10:
-        print("000" + hack)
+        print("000{}".format(hack))
     if hack > 999:
         print(hack)
     if hack == code:
-        print("The code is {}".format(code))
-        break
+        if code < 1000 and code > 99:
+            print("0{}".format(code))
+        if code < 100  and code > 9:
+            print("00{}".format(code))
+        if code < 10:
+            print("000{}".format(code))
+        if code > 999:
+            print(code)
